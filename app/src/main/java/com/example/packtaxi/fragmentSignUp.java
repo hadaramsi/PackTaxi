1f76b2d6f3145182a8147992808305ae219af253
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2022-10-25 19:43:40 +0300

    login and sign up screen ui

--- app/src/main/java/com/example/packtaxi/fragmentSignUp.java
+++ app/src/main/java/com/example/packtaxi/fragmentSignUp.java
@@ -3,10 +3,12 @@ package com.example.packtaxi;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,7 +36,7 @@ public class fragmentSignUp extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment fragmentSignUp.
+     * @return A new instance of fragment fragment_test.
      */
     // TODO: Rename and change types and number of parameters
     public static fragmentSignUp newInstance(String param1, String param2) {
@@ -59,6 +61,26 @@ public class fragmentSignUp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        Button signUpAsSenderBtn = view.findViewById(R.id.signUpAs_Sender_btn);
+        Button signUpAsDriverBtn = view.findViewById(R.id.SignUpAs_Drive_btn);
+
+        signUpAsSenderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                @NonNull NavDirections action = mainScreenFragmentDirections.actionMainScreenFragmentToSignUpFragment();
+                Navigation.findNavController(v).navigate(R.id.action_fragmentSignUp_to_fragmentSignUpAsSender);
+            }
+        });
+
+        signUpAsDriverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                @NonNull NavDirections action = MainScreenFragmentDirections.action_fragmentMainScreen_to_fragmentLogin();
+                Navigation.findNavController(v).navigate(R.id.action_fragmentSignUp_to_fragmentSignUpAsDriver);
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/packtaxi/fragmentSignUpAsDriver.java
+++ app/src/main/java/com/example/packtaxi/fragmentSignUpAsDriver.java
@@ -2,22 +2,18 @@ package com.example.packtaxi;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentMainScreen#newInstance} factory method to
+ * Use the {@link fragmentSignUpAsDriver#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentMainScreen extends Fragment {
+public class fragmentSignUpAsDriver extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +24,7 @@ public class FragmentMainScreen extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragmentMainScreen() {
+    public fragmentSignUpAsDriver() {
         // Required empty public constructor
     }
 
@@ -38,11 +34,11 @@ public class FragmentMainScreen extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentMainScreen.
+     * @return A new instance of fragment fragmentSignUpAsDriver.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentMainScreen newInstance(String param1, String param2) {
-        FragmentMainScreen fragment = new FragmentMainScreen();
+    public static fragmentSignUpAsDriver newInstance(String param1, String param2) {
+        fragmentSignUpAsDriver fragment = new fragmentSignUpAsDriver();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,27 +59,6 @@ public class FragmentMainScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View view = inflater.inflate(R.layout.fragment_main_screen, container, false);
-
-        Button signUpBtn = view.findViewById(R.id.mainScreen_btn_signUp);
-        Button loginBtn = view.findViewById(R.id.mainScreen_btn_logIn);
-
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                @NonNull NavDirections action = mainScreenFragmentDirections.actionMainScreenFragmentToSignUpFragment();
-                Navigation.findNavController(v).navigate(R.id.action_fragmentMainScreen_to_fragmentSignUp);
-            }
-        });
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                @NonNull NavDirections action = MainScreenFragmentDirections.action_fragmentMainScreen_to_fragmentLogin();
-                Navigation.findNavController(v).navigate(R.id.action_fragmentMainScreen_to_fragmentLogin);
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_sign_up_as_driver, container, false);
     }
 }
